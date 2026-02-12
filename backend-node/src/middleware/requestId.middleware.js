bacfd20e7f2ed09e9c4fa1074865fab35257bbf7
Priyanka <priyankajhamnani15@gmail.com>
2026-02-12 22:09:51 +0530

    fix: Replace uuid package with built-in crypto.randomUUID()

--- backend-node/src/middleware/requestId.middleware.js
+++ backend-node/src/middleware/requestId.middleware.js
@@ -1,7 +1,7 @@
-const { v4: uuidv4 } = require("uuid");
+const crypto = require('crypto');
 
 module.exports = (req, res, next) => {
-  req.id = req.headers["x-request-id"] || uuidv4();
+  req.id = req.headers["x-request-id"] || crypto.randomUUID();
   res.setHeader("X-Request-ID", req.id);
   next();
 };
